c66af9da39bb170d8d3b3d6c7f6ac8424ee58e41
AyushBherwani1998 <ayush.bherwani@consensys.net>
2026-03-26 14:17:22 +0530

    update smart-accounts-kit deps

--- extension/packages/nextjs/app/erc-7715-permissions/_components/PermissionBlock.tsx
+++ extension/packages/nextjs/app/erc-7715-permissions/_components/PermissionBlock.tsx
@@ -10,7 +10,7 @@ interface PermissionsListProps {
 export const PermissionBlock: React.FC<PermissionsListProps> = ({ permission }) => {
   return (
     <pre className="bg-base-200 text-base-content p-4 rounded overflow-auto text-sm font-mono h-64 border border-base-300">
-      {JSON.stringify(permission, null, 2)}
+      {JSON.stringify(permission, (_, v) => (typeof v === "bigint" ? v.toString() : v), 2)}
     </pre>
   );
 };
